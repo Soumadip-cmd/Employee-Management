b99816a72b3811c4398d7070f057a28e7c7fdf3b
Soumadip-cmd <soumadipsantra2004@gmail.com>
2024-02-26 02:53:00 +0530

    updated file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Sidebar from "./components/Sidebar";
+import Footer from "./components/Footer";
+import Dashboard from "./components/Dashboard";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Myleave from "./components/Myleave";
+import ApplyLeave from "./components/ApplyLeave";
+import Addsalary from "./components/Addsalary";
+import Managesalary from "./components/Managesalary";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Sidebar />
+        <Switch>
+          <Route exact path="/Applyleave">
+            <ApplyLeave/>
+          </Route>
+          <Route exact path="/leavehistory">
+            <Myleave/>
+          </Route>
+          <Route exact path="/addsalary">
+            <Addsalary/>
+          </Route>
+          <Route exact path="/managesalary">
+            <Managesalary/>
+          </Route>
+          <Route exact path="/">
+            <Dashboard />
+          </Route>
+        </Switch>
+        
+      </Router>
+    </>
   );
 }
 
